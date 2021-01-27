296ca45122beb1bf470ce06ed49fcc6f9e5a8366
Lisa Mittmann <li.mittmann@gmail.com>
2021-01-27 11:45:06 +0100

    Change interface to abstract class and adjust child classes

--- src/main/java/de/neuefische/testpackage/model/BiologyStudent.java
+++ src/main/java/de/neuefische/testpackage/model/BiologyStudent.java
@@ -1,46 +1,39 @@
 package de.neuefische.testpackage.model;
 
-public class BiologyStudent implements Student{
+import java.util.Objects;
 
-    private String firstName;
-    private String lastName;
-    private int matriculationNumber;
+public class BiologyStudent extends Student{
+
+    String[] modules;
 
     public BiologyStudent(String firstName, String lastName, int matriculationNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.matriculationNumber = matriculationNumber;
+        super(firstName, lastName, matriculationNumber);
     }
 
     @Override
-    public String getFirstName() {
-        return firstName;
+    public String[] getModules() {
+        return modules;
     }
 
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
-
-    @Override
-    public int getMatriculationNumber() {
-        return matriculationNumber;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setMatriculationNumber(int matriculationNumber) {
-        this.matriculationNumber = matriculationNumber;
+    public void setModules(String[] modules) {
+        this.modules = modules;
     }
 
     @Override
     public String toString() {
         return "Student name: " + this.firstName + " " + this.lastName + ", matriculation number: " + this.matriculationNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BiologyStudent that = (BiologyStudent) o;
+        return matriculationNumber == that.matriculationNumber && firstName.equals(that.firstName) && lastName.equals(that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, matriculationNumber);
     }
 }
